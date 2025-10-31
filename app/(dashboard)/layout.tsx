1cb740820f654c8233ef3b649d2a243e7d94e005
Heyru Jemal <heyru638@gmail.com>
2025-10-31 22:58:48 +0300

    rout groups

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,10 @@
+const layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div>
+      <h1>Dashboard NavBar</h1>
+      {children}
+    </div>
+  );
+};
+
+export default layout;
